d55ff9827ace853ff312fbb2989233835241dc7b
trinchero52 <trinchero52@gmail.com>
2022-08-19 21:00:07 -0300

    base de icono

--- gestionbares-frontend/src/components/Mesa/Mesa.jsx
+++ gestionbares-frontend/src/components/Mesa/Mesa.jsx
@@ -1,14 +1,21 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useRef } from "react";
 import { getMesas } from "../../services/mesa-service";
-// <a id="myLink" href="#" onclick="MyFunction();return false;">link text</a>
+import TableBarTwoToneIcon from "@mui/icons-material/TableBarTwoTone";
+import { green } from "@mui/material/colors";
+import { renderToStaticMarkup } from "react-dom/server";
+import { divIcon } from "leaflet";
+import { Marker, Popup } from "react-leaflet";
+//import "./Mesa.scss";
 
 export default function ListMesas() {
   const [mesas, setMesas] = useState([]);
   const [open, setOpen] = useState(false);
+  const [icon, setIcon] = useState([]);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const ref = useRef();
+  const toggleTooltip = () => ref.current.toggle();
 
   useEffect(() => {
     getData();
@@ -20,11 +27,27 @@ export default function ListMesas() {
     console.log(mesas);
   };
 
+  // const markers = <Marker icon={iconMesa}></Marker>;
+
+  // const iconMesa = (
+  //   <div>
+  //     <TableBarTwoToneIcon sx={{ fontSize: 130, color: green[500] }}>
+  //
+  //     </TableBarTwoToneIcon>
+  //   </div>
+  // );
+
+  // const customMesaIcon = divIcon({
+  //   html: iconMesa,
+  //   className: "dummy",
+  // });
+  // const popup = (
+
+  // );
+
   return (
     <div>
-      <a id="" href="#" onclick={handleOpen()} return false>
-        <img src="C:\Users\Tomas\mesa.png"></img>
-      </a>
+      <TableBarTwoToneIcon sx={{ fontSize: 130, color: green[500] }} />
     </div>
   );
 }
